a673cc4c6c25d0cdcbf9325bafa80cd430bbeafb
github-cloudlabsuser-089 <80265722+github-cloudlabsuser-089@users.noreply.github.com>
2023-12-13 14:55:31 +0000

    Update index.js with latest suggestion

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 // Run the following command in your terminal
@@ -18,6 +20,48 @@ const MarkdownEditor = () => {
 };
 
 export default MarkdownEditor;
+
+// reverse a sentence
+// the start of the new sentence must be capitalized
+
+
+function reverseSentence(sentence) {
+    let words = sentence.split(' ');
+    let reversedWords = [];
+    for (let i = words.length - 1; i >= 0; i--) {
+        reversedWords.push(words[i]);
+    }
+    return reversedWords.join(' ');
+}
+
+console.log(reverseSentence('I am a cat'));
+console.log(reverseSentence('I am a cat') === 'cat a am I');
+
+const data = [
+    [
+      { name: 'John', age: 25 },
+      { name: 'Jane', age: 30 }
+    ],
+    [
+      { name: 'Bob', age: 40 }
+    ]
+  ];
+
+const data = [
+        [
+            { name: 'John', age: 25 },
+            { name: 'Jane', age: 30 }
+        ],
+        [
+            { name: 'Bob', age: 40 }
+        ]
+];
+
+const names = data.flatMap(arr => arr.map(obj => obj.name));
+console.log(names);
+
+
+
 
 
 
