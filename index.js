1ad0b67be25826df3f488268a1829f2feffa9541
github-cloudlabsuser-089 <80265722+github-cloudlabsuser-089@users.noreply.github.com>
2023-12-13 15:55:19 +0000

    Fix typo

--- index.js
+++ index.js
@@ -47,16 +47,6 @@ const data = [
     ]
   ];
 
-const data = [
-        [
-            { name: 'John', age: 25 },
-            { name: 'Jane', age: 30 }
-        ],
-        [
-            { name: 'Bob', age: 40 }
-        ]
-];
-
 const names = data.flatMap(arr => arr.map(obj => obj.name));
 console.log(names);
 
